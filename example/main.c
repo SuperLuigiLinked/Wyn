77106950506cb3f90f232b29d5dfd36fba7a9801
Eric Grandizio <luigilinked@gmail.com>
2023-08-01 16:19:47 -0400

    Added example tests

--- example/main.c
+++ example/main.c
@@ -9,6 +9,78 @@
 
 // ================================================================================================================================ //
 
+[[maybe_unused]]
+static void test_time(void)
+{
+    {
+        const wyt_time_t tp1 = wyt_nanotime();
+        const wyt_time_t tp2 = wyt_nanotime();
+        const wyt_duration_t elapsed = (wyt_duration_t)(tp2 - tp1);
+        fprintf(stderr, "%lld\n", elapsed);
+    }
+
+    {
+        const wyt_time_t tp1 = wyt_nanotime();
+        wyt_yield();
+        const wyt_time_t tp2 = wyt_nanotime();
+        const wyt_duration_t elapsed = (wyt_duration_t)(tp2 - tp1);
+        fprintf(stderr, "%lld\n", elapsed);
+    }
+
+    {
+        const wyt_time_t tp1 = wyt_nanotime();
+        extern void cpp_yield(void); cpp_yield();
+        const wyt_time_t tp2 = wyt_nanotime();
+        const wyt_duration_t elapsed = (wyt_duration_t)(tp2 - tp1);
+        fprintf(stderr, "%lld\n", elapsed);
+    }
+}
+
+[[maybe_unused]]
+static void test_sleep(void)
+{
+    const wyt_time_t epoch = wyt_nanotime();
+    for (size_t i = 0; i < 5'000'000; ++i)
+    {
+        const wyt_time_t next = epoch + 16'666'666uLL * i;
+        wyt_nanosleep_until(next);
+        volatile const wyt_time_t tp = wyt_nanotime();
+        volatile const wyt_duration_t elapsed = (wyt_duration_t)(tp - epoch);
+        volatile const double millis = (double)elapsed / 1'000'000.0;
+        volatile const double frames = (double)elapsed / (1'000'000'000.0 / 60.0);
+        volatile const signed diff = (signed)frames - (signed)i;
+        fprintf(stderr, "[%7zu]: %f <%d> | %f | %10llu\n", i, frames, diff, millis, elapsed);
+        //if (millis >= 8.0) break;
+    }
+}
+
+[[maybe_unused]]
+static void thread_func(void* arg [[maybe_unused]])
+{
+    fputs("[THREAD] HELLO!\n", stderr);
+    wyt_nanosleep_for(2'000'000'000uLL);
+    fputs("[THREAD] GOODBYE!\n", stderr);
+}
+
+[[maybe_unused]]
+static void test_spawn(void) 
+{
+    fputs("[main] spawning\n", stderr);
+    const wyt_time_t tp1 = wyt_nanotime();
+    const wyt_thread_t thread = wyt_spawn(thread_func, NULL);
+    const wyt_time_t tp2 = wyt_nanotime();
+    const wyt_duration_t elapsed = (wyt_duration_t)(tp2 - tp1);
+    fprintf(stderr, "SPAWN: %lld\n", elapsed);
+    if (!thread) return;
+
+    fputs("[main] joining\n", stderr);
+    wyt_join(thread);
+    fputs("[main] terminating\n", stderr);
+}
+
+[[maybe_unused]]
+extern void test_cpp(void);
+
 // int WINAPI wWinMain
 // (
 //     _In_ HINSTANCE hInstance,
@@ -18,24 +90,19 @@
 // )
 int main(void)
 {
-    extern void test_cpp(void);
-    test_cpp();
+    test_time();
+    //test_sleep();
+    //test_spawn();
+    //test_cpp();
     return 0;
 
-    wyt_thread_t thread;
+    // wyt_thread_t thread;
 
-    fputs("[START]\n", stderr);
-    const int code = wyn_run(&thread);
-    fputs("[STOP]\n", stderr);
+    // fputs("[START]\n", stderr);
+    // const int code = wyn_run(&thread);
+    // fputs("[STOP]\n", stderr);
 
-    return code;
-}
-
-static void thread_func(void* arg [[maybe_unused]])
-{
-    fputs("[THREAD] Hello!\n", stderr);
-    wyt_nanosleep_for(5'000'000'000uLL);
-    fputs("[THREAD] Goodbye!\n", stderr);
+    // return code;
 }
 
 // ================================================================================================================================ //
--- example/test.cpp
+++ example/test.cpp
@@ -4,18 +4,36 @@
 
 #include <time.h>
 #include <chrono>
+#include <thread>
+#include <latch>
 
 // ================================================================================================================================ //
 
-extern "C" void test_cpp(void);
+extern "C" void cpp_yield(void);
+extern "C" void cpp_yield(void)
+{
+    std::this_thread::yield();
+}
 
+extern "C" void test_cpp(void);
 extern "C" void test_cpp(void)
 {
+    {
+        std::latch latch{ 1 };
+        latch.arrive_and_wait();
+
+
+        std::thread{ cpp_yield }.detach();
+
+        std::jthread{ cpp_yield }.detach();
+    }
+
     [[maybe_unused]] volatile time_t a = time(nullptr);
     [[maybe_unused]] volatile clock_t b = clock();
     [[maybe_unused]] volatile auto c = std::chrono::steady_clock::now();
     [[maybe_unused]] volatile auto d = std::chrono::high_resolution_clock::now();
     [[maybe_unused]] volatile auto e = std::chrono::system_clock::now();
+    std::this_thread::yield();
 }
 
 // ================================================================================================================================ //
