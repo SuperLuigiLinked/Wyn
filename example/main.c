9edfdbebfff482a991d7f2222b1600b3eb7ef672
Eric Grandizio <luigilinked@gmail.com>
2023-08-02 15:19:38 -0400

    Modified test program

--- example/main.c
+++ example/main.c
@@ -39,18 +39,28 @@ static void test_time(void)
 [[maybe_unused]]
 static void test_sleep(void)
 {
-    const wyt_time_t epoch = wyt_nanotime();
-    for (size_t i = 0; i < 5'000'000; ++i)
+    const wyt_duration_t dur = 500;
+    //const wyt_duration_t dur = 1'000'000'000;
+    fprintf(stderr, "Expected: %10lld | %+10lld\n", dur, 0LL);
+
     {
-        const wyt_time_t next = epoch + 16'666'666uLL * i;
-        wyt_nanosleep_until(next);
-        volatile const wyt_time_t tp = wyt_nanotime();
-        volatile const wyt_duration_t elapsed = (wyt_duration_t)(tp - epoch);
-        volatile const double millis = (double)elapsed / 1'000'000.0;
-        volatile const double frames = (double)elapsed / (1'000'000'000.0 / 60.0);
-        volatile const signed diff = (signed)frames - (signed)i;
-        fprintf(stderr, "[%7zu]: %f <%d> | %f | %10llu\n", i, frames, diff, millis, elapsed);
-        //if (millis >= 8.0) break;
+        const wyt_time_t tp1 = wyt_nanotime();
+        wyt_nanosleep_for(dur);
+        const wyt_time_t tp2 = wyt_nanotime();
+        const wyt_duration_t elapsed = (wyt_duration_t)(tp2 - tp1);
+        const wyt_time_t expected = tp1 + (wyt_time_t)dur;
+        const wyt_duration_t diff = (wyt_duration_t)(tp2 - expected);
+        fprintf(stderr, "Sleep F.: %10lld | %+10lld\n", elapsed, diff);
+    }
+    
+    {
+        const wyt_time_t tp1 = wyt_nanotime();
+        const wyt_time_t expected = tp1 + (wyt_time_t)dur;
+        wyt_nanosleep_until(expected);
+        const wyt_time_t tp2 = wyt_nanotime();
+        const wyt_duration_t elapsed = (wyt_duration_t)(tp2 - tp1);
+        const wyt_duration_t diff = (wyt_duration_t)(tp2 - expected);
+        fprintf(stderr, "Sleep U.: %10lld | %+10lld\n", elapsed, diff);
     }
 }
 
@@ -79,6 +89,24 @@ static void test_spawn(void)
 }
 
 [[maybe_unused]]
+static void test_frames(void)
+{
+    const wyt_time_t epoch = wyt_nanotime();
+    for (size_t i = 0; i < 5'000'000; ++i)
+    {
+        const wyt_time_t next = epoch + 16'666'666uLL * i;
+        wyt_nanosleep_until(next);
+        volatile const wyt_time_t tp = wyt_nanotime();
+        volatile const wyt_duration_t elapsed = (wyt_duration_t)(tp - epoch);
+        volatile const double millis = (double)elapsed / 1'000'000.0;
+        volatile const double frames = (double)elapsed / (1'000'000'000.0 / 60.0);
+        volatile const signed diff = (signed)frames - (signed)i;
+        fprintf(stderr, "[%7zu]: %f <%d> | %f | %10llu\n", i, frames, diff, millis, elapsed);
+        //if (millis >= 8.0) break;
+    }
+}
+
+[[maybe_unused]]
 extern void test_cpp(void);
 
 // int WINAPI wWinMain
@@ -93,6 +121,7 @@ int main(void)
     test_time();
     //test_sleep();
     //test_spawn();
+    //test_frames();
     //test_cpp();
     return 0;
 
