5dda4cf18d8316e69fca858bf9de65b9f6dc892a
Eric Grandizio <luigilinked@gmail.com>
2023-08-05 00:46:11 -0400

    Testing -exec functionality

--- example/main.c
+++ example/main.c
@@ -10,33 +10,15 @@
 
 // ================================================================================================================================ //
 
-[[maybe_unused]]
-extern void test_cpp(void);
-
-static void thread_func(void* arg [[maybe_unused]])
+struct AppState
 {
-    fputs("[THREAD] HELLO!\n", stderr);
-    wyt_nanosleep_for(5'000'000'000uLL);
-    fputs("[THREAD] GOODBYE!\n", stderr);
-}
+    wyt_thread_t thread;
+    wyn_window_t window;
+};
 
-static void test_tid_2(void* arg)
-{
-    const wyt_tid_t tid = wyt_current_tid();
-    const wyt_tid_t mid = *(const wyt_tid_t*)arg;
-    fprintf(stderr, "[THRD] %llu\n", tid);
-    fprintf(stderr, "[MAIN] %llu\n", mid);
-}
+#define ASSERT(expr) if (expr) {} else abort()
 
-static void test_tid(void)
-{
-    wyt_tid_t tid = wyt_current_tid();
-    fprintf(stderr, "[MAIN] %llu\n", tid);
-
-    const wyt_thread_t thread = wyt_spawn(test_tid_2, &tid);
-    if (!thread) abort();
-    wyt_join(thread);
-}
+[[maybe_unused]] extern void test_cpp(void);
 
 // ================================================================================================================================ //
 
@@ -49,56 +31,98 @@ static void test_tid(void)
 // )
 int main(void)
 {
-    const wyt_time_t tp1 = wyt_nanotime();
-    test_tid();
-    const wyt_time_t tp2 = wyt_nanotime();
-    const wyt_duration_t elapsed = (wyt_duration_t)(tp2 - tp1);
-    fprintf(stderr, "[time] %lld ns\n", elapsed);
-    return 0;
-
-    wyt_thread_t thread = NULL;
+    struct AppState state = {
+        .thread = NULL,
+        .window = NULL,
+    };
 
     fputs("[START]\n", stderr);
-    const int code = wyn_run(&thread);
+    wyn_run(&state);
     fputs("[STOP]\n", stderr);
 
-    return code;
+    return 0;
+}
+
+// ================================================================================================================================ //
+
+static void print_tid(void* label)
+{
+    const wyt_time_t tp = wyt_nanotime();
+
+    const wyt_tid_t tid = wyt_current_tid();
+    fprintf(stderr, "[TID %s] %llu\t\t(%llu)\n", (const char*)label, tid, tp);
+}
+
+static void quit_async(void*)
+{
+    wyn_quit();
+}
+
+static void thread_func(void* arg)
+{
+    struct AppState* state = (struct AppState*)arg;
+    ASSERT(state != NULL);
+
+    print_tid("THREAD 1");
+    wyn_execute(print_tid, "THREAD 2");
+    wyn_execute_async(print_tid, "THREAD 3");
+
+    wyn_execute_async(quit_async, NULL);
 }
 
 // ================================================================================================================================ //
 
 extern void wyn_on_start(void* userdata)
 {
-    (void)userdata;
+    fputs("[WYN] <START>\n", stderr);
 
-    fputs("[WYN - START]\n", stderr);
+    struct AppState* state = (struct AppState*)userdata;
+    ASSERT(state != NULL);
 
-    const wyn_window_t window = wyn_open_window();
-    if (!window) { wyn_quit(1); return; }
+    state->window = wyn_open_window();
+    if (!state->window) { wyn_quit(); return; }
+    wyn_show_window(state->window);
 
-    wyn_show_window(window);
+    print_tid("MAIN 1");
+    wyn_execute(print_tid, "MAIN 2");
+    wyn_execute_async(print_tid, "MAIN 3");
 
-    wyt_thread_t* thread = (wyt_thread_t*)userdata;
-    *thread = wyt_spawn(thread_func, NULL);
+    state->thread = wyt_spawn(thread_func, userdata);
+    if (!state->thread) { wyn_quit(); return; }
 }
 
 extern void wyn_on_stop(void* userdata)
 {
-    (void)userdata;
+    fputs("[WYN] <STOP>\n", stderr);
 
-    fputs("[WYN - STOP]\n", stderr);
+    struct AppState* state = (struct AppState*)userdata;
+    ASSERT(state != NULL);
 
-    wyt_thread_t* thread = (wyt_thread_t*)userdata;
-    if (*thread != NULL) wyt_join(*thread);
+    if (state->thread)
+    {
+        wyt_join(state->thread);
+        state->thread = NULL;
+    }
+    
+    if (state->window)
+    {
+        wyn_close_window(state->window);
+        state->window = NULL;
+    }
 }
 
 extern void wyn_on_window_close(void* userdata, wyn_window_t window)
 {
-    (void)userdata; (void)window;
+    fputs("[WYN] <CLOSE>\n", stderr);
 
-    fputs("[WYN - CLOSE]\n", stderr);
-    
-    wyn_quit(0);
+    struct AppState* state = (struct AppState*)userdata;
+    ASSERT(state != NULL);
+
+    if (state->window == window)
+    {
+        wyn_quit();
+        state->window = NULL;
+    }    
 }
 
 // ================================================================================================================================ //
