71c8afab0512b5104ea257ffce36b61991e4d0bf
Eric Grandizio <luigilinked@gmail.com>
2023-08-11 14:16:18 -0400

    Restructured project

--- example/src/main.c
+++ example/src/main.c
@@ -2,49 +2,17 @@
  * @file example/main.c
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <wyn.h>
-#include <wyt.h>
+#include "App.h"
 
 // ================================================================================================================================
-
-#define ASSERT(expr) if (expr) {} else abort()
-
-#define LOG(...) (void)fprintf(stderr, __VA_ARGS__)
 
 [[maybe_unused]] extern void test_cpp(void);
 
 // ================================================================================================================================
 
-struct AppState
-{
-    wyt_thread_t thread;
-    wyn_window_t window;
-};
-
-// ================================================================================================================================
-
-[[maybe_unused]]
-static void quit_async(void*)
-{
-    wyn_quit();
-}
-
-static wyt_retval_t WYT_ENTRY thread_func(void* arg)
-{
-    [[maybe_unused]] struct AppState* const state = arg;
-
-    //wyn_execute_async(quit_async, NULL);
-    return 0;
-}
-
-// ================================================================================================================================
-
 extern void wyn_on_start(void* userdata)
 {
-    fputs("[WYN] <START>\n", stderr);
+    LOG("[WYN] <START>\n");
 
     struct AppState* state = (struct AppState*)userdata;
     ASSERT(state != NULL);
@@ -53,13 +21,15 @@ extern void wyn_on_start(void* userdata)
     if (!state->window) { wyn_quit(); return; }
     wyn_show_window(state->window);
 
-    state->thread = wyt_spawn(thread_func, userdata);
+    state->thread = wyt_spawn(gfx_func, userdata);
     if (!state->thread) { wyn_quit(); return; }
 }
 
+// --------------------------------------------------------------------------------------------------------------------------------
+
 extern void wyn_on_stop(void* userdata)
 {
-    fputs("[WYN] <STOP>\n", stderr);
+    LOG("[WYN] <STOP>\n");
 
     struct AppState* state = (struct AppState*)userdata;
     ASSERT(state != NULL);
@@ -77,9 +47,11 @@ extern void wyn_on_stop(void* userdata)
     }
 }
 
+// --------------------------------------------------------------------------------------------------------------------------------
+
 extern void wyn_on_window_close(void* userdata, wyn_window_t window)
 {
-    fputs("[WYN] <CLOSE>\n", stderr);
+    LOG("[WYN] <CLOSE>\n");
 
     struct AppState* state = (struct AppState*)userdata;
     ASSERT(state != NULL);
@@ -105,27 +77,15 @@ int WINAPI wWinMain
 int main(void)
 #endif
 {
-    {
-        struct AppState state = {
-            .thread = NULL,
-            .window = NULL,
-        };
+    struct AppState state = {
+        .thread = NULL,
+        .window = NULL,
+    };
 
-        LOG("[START]\n");
-        wyn_run(&state);
-        LOG("[STOP]\n");
-    }
-    LOG("\n");
-    {
-        struct AppState state = {
-            .thread = NULL,
-            .window = NULL,
-        };
+    LOG("[START]\n");
+    wyn_run(&state);
+    LOG("[STOP]\n");
 
-        LOG("[START]\n");
-        wyn_run(&state);
-        LOG("[STOP]\n");
-    }
     return 0;
 }
 
