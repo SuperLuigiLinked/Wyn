2470504894a588f86d6415ccb7909e699549519d
Eric <luigilinked@gmail.com>
2023-08-15 16:24:08 -0400

    Added OpenGL headers

--- wyg/include/wyg_gl.h
+++ wyg/include/wyg_gl.h
@@ -2,8 +2,23 @@
  * @file wyg_gl.h
  */
 
+#if defined(__APPLE__)
+    #include <OpenGL/OpenGL.h>
+    #include <OpenGL/gl.h>
+#elif defined(_WIN32)
+    #include <Windows.h>
+    #include <gl/GL.h>
+#else
+    #include <GL/glx.h>
+    #include <GL/gl.h>
+#endif
+
 // ================================================================================================================================
 
-extern void wyg_gl_create(void);
+extern void wyg_gl_create_context(void);
+
+extern void wyg_gl_make_current(void);
+
+extern void wyg_gl_destroy_context(void);
 
 // ================================================================================================================================
