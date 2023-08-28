9b248eaed6558f9f3e121172bd8ffcfa61b66969
Eric Grandizio <luigilinked@gmail.com>
2023-08-28 17:20:38 -0400

    New semaphore test

--- example/src/main.c
+++ example/src/main.c
@@ -12,23 +12,6 @@ struct Test
 };
 typedef struct Test Test;
 
-static wyt_retval_t WYT_ENTRY test_thread(void* arg)
-{
-    LOG("[TEST] START\n");
-    {
-        Test* const test = arg;
-        ASSERT(test != 0);
-
-        LOG("[TEST] SLEEPING\n");
-        wyt_nanosleep_for(2'000'000'000ull);
-
-        LOG("[TEST] RELEASING\n");
-        wyt_sem_release(test->sem);
-    }
-    LOG("[TEST] STOP\n");
-    return 0;
-}
-
 static void test_sem(void)
 {
     Test test = {};
@@ -37,16 +20,9 @@ static void test_sem(void)
     test.sem = wyt_sem_create(1, 0);
     ASSERT(test.sem != 0);
     {
-        LOG("[MAIN] SPAWNING\n");
-        const wyt_thread_t thread = wyt_spawn(test_thread, &test);
-        ASSERT(thread != 0);
-        {
-            LOG("[MAIN] ACQUIRING\n");
-            wyt_sem_acquire(test.sem);
-        }
-
-        LOG("[MAIN] JOINING\n");
-        (void)wyt_join(thread);
+        ASSERT(false == wyt_sem_try_acquire(test.sem));
+        wyt_sem_release(test.sem);
+        ASSERT(true == wyt_sem_try_acquire(test.sem));
     }
     LOG("[MAIN] DESTROYING\n");
     wyt_sem_destroy(test.sem);
