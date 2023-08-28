f14c3d89bb51e92dd566b1316d34646963c7c33f
Eric Grandizio <luigilinked@gmail.com>
2023-08-28 16:26:20 -0400

    Added temporary semaphore test

--- example/src/main.c
+++ example/src/main.c
@@ -6,6 +6,54 @@
 
 // ================================================================================================================================
 
+struct Test
+{
+    wyt_sem_t sem;
+};
+typedef struct Test Test;
+
+static wyt_retval_t WYT_ENTRY test_thread(void* arg)
+{
+    LOG("[TEST] START\n");
+    {
+        Test* const test = arg;
+        ASSERT(test != 0);
+
+        LOG("[TEST] SLEEPING\n");
+        wyt_nanosleep_for(2'000'000'000ull);
+
+        LOG("[TEST] RELEASING\n");
+        wyt_sem_release(test->sem);
+    }
+    LOG("[TEST] STOP\n");
+    return 0;
+}
+
+static void test_sem(void)
+{
+    Test test = {};
+
+    LOG("[MAIN] CREATING\n");
+    test.sem = wyt_sem_create(1, 0);
+    ASSERT(test.sem != 0);
+    {
+        LOG("[MAIN] SPAWNING\n");
+        const wyt_thread_t thread = wyt_spawn(test_thread, &test);
+        ASSERT(thread != 0);
+        {
+            LOG("[MAIN] ACQUIRING\n");
+            wyt_sem_acquire(test.sem);
+        }
+
+        LOG("[MAIN] JOINING\n");
+        (void)wyt_join(thread);
+    }
+    LOG("[MAIN] DESTROYING\n");
+    wyt_sem_destroy(test.sem);
+}
+
+// ================================================================================================================================
+
 #if defined(_WIN32) && 0
 int WINAPI wWinMain
 (
@@ -20,7 +68,8 @@ int main(void)
 {
     LOG("[START]\n");
     {
-        events_loop();
+        test_sem();
+        // events_loop();
     }
     LOG("[STOP]\n");
     return 0;
