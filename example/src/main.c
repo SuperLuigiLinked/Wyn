4a0e2fc67ab4206dfc81e237246368b0a541cc9c
Eric Grandizio <65352263+SuperLuigiLinked@users.noreply.github.com>
2023-08-28 17:22:38 -0400

    Removed test

--- example/src/main.c
+++ example/src/main.c
@@ -6,30 +6,6 @@
 
 // ================================================================================================================================
 
-struct Test
-{
-    wyt_sem_t sem;
-};
-typedef struct Test Test;
-
-static void test_sem(void)
-{
-    Test test = {};
-
-    LOG("[MAIN] CREATING\n");
-    test.sem = wyt_sem_create(1, 0);
-    ASSERT(test.sem != 0);
-    {
-        ASSERT(false == wyt_sem_try_acquire(test.sem));
-        wyt_sem_release(test.sem);
-        ASSERT(true == wyt_sem_try_acquire(test.sem));
-    }
-    LOG("[MAIN] DESTROYING\n");
-    wyt_sem_destroy(test.sem);
-}
-
-// ================================================================================================================================
-
 #if defined(_WIN32) && 0
 int WINAPI wWinMain
 (
@@ -44,8 +20,7 @@ int main(void)
 {
     LOG("[START]\n");
     {
-        test_sem();
-        // events_loop();
+        events_loop();
     }
     LOG("[STOP]\n");
     return 0;
