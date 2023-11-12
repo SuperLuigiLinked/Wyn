9a492e1438e715932726f45f25095afefe0f9b64
Eric <luigilinked@gmail.com>
2023-11-12 17:12:13 -0500

    Modified stub linkage

--- wyn/src/wyn_stubs.c
+++ wyn/src/wyn_stubs.c
@@ -7,18 +7,10 @@
 
 // ================================================================================================================================
 
-#pragma weak wyn_on_start
-#pragma weak wyn_on_stop
-#pragma weak wyn_on_signal
-#pragma weak wyn_on_window_close
-#pragma weak wyn_on_window_redraw
-
-// ================================================================================================================================
-
-extern void wyn_on_start(void* userdata)                              { (void)userdata; }
-extern void wyn_on_stop(void* userdata)                               { (void)userdata; }
-extern void wyn_on_signal(void* userdata)                             { (void)userdata; }
-extern void wyn_on_window_close(void* userdata, wyn_window_t window)  { (void)userdata; wyn_window_close(window); wyn_quit(); }
-extern void wyn_on_window_redraw(void* userdata, wyn_window_t window) { (void)userdata; (void)window; }
+extern void __attribute__((weak)) wyn_on_start(void* userdata)                              { (void)userdata; }
+extern void __attribute__((weak)) wyn_on_stop(void* userdata)                               { (void)userdata; }
+extern void __attribute__((weak)) wyn_on_signal(void* userdata)                             { (void)userdata; }
+extern void __attribute__((weak)) wyn_on_window_close(void* userdata, wyn_window_t window)  { (void)userdata; wyn_window_close(window); wyn_quit(); }
+extern void __attribute__((weak)) wyn_on_window_redraw(void* userdata, wyn_window_t window) { (void)userdata; (void)window; }
 
 // ================================================================================================================================
